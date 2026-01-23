5f7ccae1d7dd4486829a86be529d5cf4a798a7ad
Vidit Aggarwal <99411225+viditforsv@users.noreply.github.com>
2026-01-24 00:01:16 +0530

    Fix build errors: exclude backup folders from TypeScript and webpack

--- next.config.js
+++ next.config.js
@@ -4,5 +4,19 @@ const withNextra = require('nextra')({
   latex: true,
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  // Exclude backup folders from build
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules/**',
+        '**/backup_*/**',
+        '**/pages_backup*/**',
+        '**/.next/**',
+      ],
+    }
+    return config
+  },
+})
 
